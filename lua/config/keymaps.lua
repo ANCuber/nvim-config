969e0cc0b9f3d4fefef71f115416e1073f957e53
ancuber1031 <ansona1131@gmail.com>
2025-02-19 20:28:29 +0800

    mdf cp setting

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@ vim.keymap.set("i", "jk", "<ESC>")
 vim.keymap.set("n", "<TAB>", "i<TAB>")
 
 -- For competitive programming only
-vim.keymap.set("n", "<F12>", ":r ~/CppCode/cp_code/setting.cpp<CR>ggdd8jo")
+vim.keymap.set("n", "<F12>", ":r ~/Coding/CppCode/cp_code/setting.cpp<CR>ggdd8jo")
 
 -- Open a terminal at the buttom of the window
 vim.keymap.set("n", "<leader>t", ":belowright 10split | terminal<CR>i", { noremap = true, silent = true })
