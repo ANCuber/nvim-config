c02af452885e0c774707a28acd93111908869424
ancuber1031 <ansona1131@gmail.com>
2025-02-25 23:25:51 +0800

    tab

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,9 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- search the filename
-    vim.keymap.set('n', '<leader>fw', builtin.live_grep, {}) -- search the word 
-  end
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {}) -- search the filename
+        vim.keymap.set('n', '<leader>fw', builtin.live_grep, {}) -- search the word 
+    end
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":tsupdate", -- use the command
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua", "cpp", "python"},
-      highlight = { enable = true },
-      indent = { enable = true }, -- auto indent after brackets
-    })
-  end
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate", -- use the command
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = {"lua", "cpp", "python"},
+            highlight = { enable = true },
+            indent = { enable = true }, -- auto indent after brackets
+        })
+    end
 }
