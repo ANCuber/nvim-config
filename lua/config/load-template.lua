0a58c0f8fb4864a490e55528f2761ba93799f53f
ancuber1031 <ansona1131@gmail.com>
2025-09-12 17:24:05 +0800

    Change to defaults & add default.c

--- lua/config/load-template.lua
+++ lua/config/load-template.lua
@@ -4,15 +4,20 @@ function LoadTemplate()
 	local filetype = vim.bo.filetype
 
     if filetype == "cpp" then
-        vim.cmd("r ~/Documents/Templates/Cpp/temp.cpp")
+        vim.cmd("r ~/Documents/Defaults/Cpp/default.cpp")
         vim.cmd("normal! ggdd")
-        vim.cmd("7");
+        vim.cmd("7")
+        vim.api.nvim_feedkeys("o", "n", false)
+    elseif filetype == "c" then
+        vim.cmd("r ~/Documents/Defaults/C/default.c")
+        vim.cmd("normal! ggdd")
+        vim.cmd("3")
         vim.api.nvim_feedkeys("o", "n", false)
 	elseif filetype == "plaintex" or filetype == "tex" then
-	    vim.cmd("r ~/Documents/Templates/Latex/temp.tex")
+	    vim.cmd("r ~/Documents/Defaults/Latex/default.tex")
         vim.cmd("normal! ggdd")
     elseif filetype == "typst" then
-        vim.cmd("r ~/Documents/Templates/Typst/temp.typ")
+        vim.cmd("r ~/Documents/Defaults/Typst/default.typ")
         vim.cmd("normal! ggdd")
     end
 end
